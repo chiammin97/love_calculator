171dd1fcbe6b67d4195ff8589e02c78711e0e70b
chiammin97 <chiammin@gmail.com>
2020-04-18 23:09:12 +0800

    -

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+alert("Please Enter Name");
+
+function main() {
+
+const name = document.getElementById("yourName")
+const name2 = document.getElementById("otherName")
+const form = document.getElementById("myForm")
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+    
+    e.preventDefault()
+
+    
+    if (name.value === '' || name.value == null) {
+        //errorElement.innerText = ("Please Enter Name");
+        //alert("Please Enter Name");
+        return
+    }
+}
+
 function btn() {
   document.getElementById("disappear").innerHTML = "";
   document.getElementById("number").innerHTML =
     Math.floor(Math.random() * 100) + "%";
 }
+
+}
+
+document.onload = main
